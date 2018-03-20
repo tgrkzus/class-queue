b37acc56527c93f4ea2f1c2bc38f8787db53797a
Abhi Jagtap <abhijagtap1997@gmail.com>
2018-03-20 14:41:20 +1000

    Websocket test

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template
 app = Flask(__name__, static_folder="static")
 
+socketio = SocketIO(app)
+
+
+@socketio.on('json')
+def handle_json(json):
+    print("received json: " + str(json))
+
 @app.route("/")
 def hello():
+    print("Helo World")
     return render_template("index.html")
-
