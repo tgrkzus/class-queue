2dd5596d388e3686be8ef7b86d2a1a547ec49b61
Abhi Jagtap <abhijagtap1997@gmail.com>
2018-03-20 15:13:03 +1000

    Added boradcasting and queue logic

--- app.py
+++ app.py
@@ -1,14 +1,31 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO
 app = Flask(__name__, static_folder="static")
 
 socketio = SocketIO(app)
 
+## the currentId of the user
+currentId = 0
 
-@socketio.on('json')
-def handle_json(json):
+## Current Queue
+queue = []
+
+## name: "Firtname LastName"
+
+@socketio.on('add_name')
+def add_name(json):
+    studentid = currentId
+    currentId += 1
+    queue.append({"id": studentid, "name": json["name"]})
     print("received json: " + str(json))
+
+@socketio.on('remove_name')
+def remove_name(id):
+    for i in range(0,len(queue)):
+        if id == queue[i]["id"]:
+            emit('remove_name', queue.pop(i), boradcast = True)
 
 @app.route("/")
 def hello():
-    print("Helo World")
+    print("Connected!!")
     return render_template("index.html")
