cd19936ef9b63de3b3f0f738fa16094487ce5f1a
Tom Richardson <tgrkzus@gmail.com>
2018-03-20 14:38:44 +1000

    requirements + some test setup for websockets

--- static/queue.js
+++ static/queue.js
@@ -1,3 +1,29 @@
-function test() {
-    console.log("hey");
+short_names = []
+long_names = []
+
+function setup() {
+    var socket = new WebSocket("ws://" + location.host + "/data")
+
+    socket.onmessage = function(event) {
+        console.log(event.data);
+    }
+}
+
+var add_name = function(type, id, name) {
+    if (type == "short") {
+        short_names.push([id, name]);
+    }
+
+    if (type == "long") {
+        short_names.push([id, name]);
+    }
+}
+
+var remove_name = function(type, id) {
+    if (type == "short") {
+
+    }
+    if (type == "long") {
+
+    }
 }
